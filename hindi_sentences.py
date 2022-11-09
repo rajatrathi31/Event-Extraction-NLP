f0c1cd40c22fbde8730c3c78a2ee87005da37903
Soutik Gayen <soutikgayen2409@gmail.com>
2022-11-09 21:56:51 +0530

    update sentence generation code

--- hindi_sentences.py
+++ hindi_sentences.py
@@ -2,37 +2,11 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 import os
 
-# sentences = []
-# s = ""
-
-# def extract(item) :
-#     for child in item :
-#         # print(child.tag)
-#         if(child.tag == "P") :
-#             if(len(s) != 0) :
-#                 sentences.append(s)
-#         elif(child.tag == "W"):
-#             # s = s + " " + child.text
-#             print(child.text)
-#         else:
-#             extract(child)
-#     # sentences.append(s)
 
 path = "hindi_ee/test/1-5-lac-property-ashes-in-a-fire-caused-by-short-circuit-hindi-news5.txt.xml"
 tree = ET.parse(path)
 root = tree.getroot()
 
-# extract(root)
-
-# print(sentences)
-
-# for item in root.findall("P") :
-# for j in root.findall("W") :
-#     print(j.text)
-# # print(root.findall("W"))
-
-# for child in root :
-#     print(child.attrib)
 
 file_path = "./test"
 sentences = []
